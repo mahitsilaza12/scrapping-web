b70ee4bdfcc5f30a03054d6b3d13fc18fd4bfa9a
Fabriola <fabriola@hightao-mg.com>
2024-06-20 00:45:46 +0300

    test correct mais avoir une erreur waring apres la test en success

--- tests/NewsControllerTest.php
+++ tests/NewsControllerTest.php
@@ -2,22 +2,26 @@
 
 namespace App\Tests;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Panther\PantherTestCase;
 
-class NewsControllerTest extends WebTestCase
+class NewsControllerTest extends PantherTestCase
 {
     public function testNews()
     {
-        $client = static::createClient();
+        $client = static::createPantherClient();
         $crawler = $client->request('GET', '/');
 
+        // Vérifie qu'il y a deux éléments <h1> sur la page
         $this->assertCount(2, $crawler->filter('h1'));
+
+        // Modifie pour passer un tableau contenant 'id'
         $this->assertSame(['week-601', 'symfony-live-usa-2018'], $crawler->filter('article')->extract(['id']));
 
+        // Sélectionne un lien par son texte et clique dessus
         $link = $crawler->selectLink('Join us at SymfonyLive USA 2018!')->link();
         $crawler = $client->click($link);
 
+        // Vérifie le texte du <h1> après avoir cliqué sur le lien
         $this->assertSame('Join us at SymfonyLive USA 2018!', $crawler->filter('h1')->text());
     }
 }
-
